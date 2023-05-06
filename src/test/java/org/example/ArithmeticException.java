8c6815beb4e97be0ef43c94e82c1e48e532348d6
Darija Paulska <paulskadarija@gmail.com>
2023-05-06 20:34:31 +0300

    Homework number 20

--- src/test/java/org/example/ArithmeticException.java
+++ src/test/java/org/example/ArithmeticException.java
@@ -1,2 +1,54 @@
-package org.example;public class ArithmeticException {
-}
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ArithmeticException {
+
+    @Test
+    public void testException() {
+        assertThrows(java.lang.ArithmeticException.class, () -> {
+            divideTwoNumbers(10, 0);
+        });
+
+    }
+
+    @Test
+    public void testDivideTwoNumbersReturnsCorrectResult() {
+        assertEquals(3, divideTwoNumbers(18, 6));
+    }
+
+    private int divideTwoNumbers(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+
+    //EX.3
+
+    @Test
+    public void positiveTestWithMassive() {
+        int[] myArray = {1, 2, 3, 4, 5} ;
+        int arrayElement = myArray[3];
+        Assertions.assertEquals(arrayElement, 4);
+    }
+
+    @Test
+    public void negativeTestWithMassiveException(){
+        int[] myArray = {1, 2, 3, 4, 5};
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            int arrayElement = myArray[10];
+            System.out.println("Array index out of massive");
+        });
+    }
+
+
+    }
+
+
+
+
+
+
+
--- src/test/java/org/example/Factorial.java
+++ src/test/java/org/example/Factorial.java
@@ -1,2 +1,39 @@
-package org.example;public class Афсещкшфд {
+package org.example;
+
+import java.math.BigInteger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = null;
+        try {
+            System.out.print("Enter an integer: ");
+            scanner = new Scanner(System.in);
+
+            int n = scanner.nextInt();
+            BigInteger factorial = BigInteger.valueOf(1);
+            for (int i = 2; i <= n; i++) {
+                factorial = factorial.multiply(BigInteger.valueOf(i));
+            }
+
+            System.out.println("Result: " + n + "! " + " = " + factorial.toString());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: Negative number!");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Should be only numbers! ");
+
+        } finally {
+            scanner.close();
+
+        }
+    }
+
+
+
 }
+
+
